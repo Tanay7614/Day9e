dcea0525c483e8c246a41ac8d82038d9555c4f38
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-19 22:15:21 +0530

    Add files via upload

---
+++ Dailywage.java
@@ -0,0 +1,13 @@
+package Uc1;
+
+public class Dailywage {
+		   public static void main(String[] args){
+		     int wage_perhour=20;
+		     int Total_hour=8;
+		     int daily_emp_wage=wage_perhour*Total_hour;
+		     System.out.println("Daily employee wage=" + daily_emp_wage);
+		}
+		}
+
+
+
