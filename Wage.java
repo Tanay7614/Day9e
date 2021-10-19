6e5de3fd4de1d98f5c8c94d80d58408686db27f8
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-19 22:12:41 +0530

    Add files via upload

---
+++ Wage.java
@@ -0,0 +1,16 @@
+package Uc1;
+
+public class Wage {
+		 public static void main(String[] args){
+		  final int Emp_Check=1;
+		  double empcheck=Math.floor(Math.random()*10)%2;
+		  if(Emp_Check==empcheck){
+		    System.out.println("employyed is present");
+		 }
+		else{
+		  System.out.println("Employyed is absent");
+		}
+		}
+		}
+
+
